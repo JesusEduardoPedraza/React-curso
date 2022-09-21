2541509f096515878c0c458f666b8c27beba0a92
JESUS EDUARDO <j_edi_24@hotmail.com>
2022-09-21 18:04:59 -0500

    agregamos css

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,13 +1,37 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 export const TaskContext = createContext();
+import { tasks as data } from "../data/Task";
 
 export function TaskContextProvider(props) {
+  const [tasks, setTasks] = useState([]);
 
-    let x =20
+  function createTask(task) {
+    setTasks([
+      ...tasks,
+      {
+        title: task.title,
+        id: tasks.length,
+        description: task.description,
+      },
+    ]);
+  }
 
-  return (<TaskContext.Provider value ={x}> 
-    {props.children}
+  function deleteTask(taskId) {
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  }
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  return (
+    <TaskContext.Provider
+      value={{
+        tasks,
+        createTask,
+        deleteTask,
+      }}
+    >
+      {props.children}
     </TaskContext.Provider>
-    )
+  );
 }
-

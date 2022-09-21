4e9ca46765695516edf0a26e573fa7505300aaeb
JESUS EDUARDO <j_edi_24@hotmail.com>
2022-09-21 14:34:49 -0500

    añadir datos desde formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import TaskList from './TaskList'
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import { tasks as data } from "./data/Task";
+import { useState, useEffect } from "react";
 
-function App (){
-  return <div>
-    <TaskList/>
-  </div>
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(task){
+    setTasks([...tasks,{
+      title: task.title,
+      id:tasks.length,
+      description: task.description
+    }])
+  }
+
+  function deleteCard (TaskId){
+
+  }
+
+  return (
+    <>
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} deleteCard={deleteCard}/>
+    </>
+  );
 }
 
-export default App
+export default App;
